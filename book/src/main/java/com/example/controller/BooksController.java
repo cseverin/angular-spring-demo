908955cb8220d138667c0315ce310de211dbebf0
cseverin <carstenseverin@web.de>
2022-04-07 13:17:12 +0200

    Log-Ausgaben

--- book/src/main/java/com/example/controller/BooksController.java
+++ book/src/main/java/com/example/controller/BooksController.java
@@ -5,11 +5,14 @@ import com.example.model.Book;
 import com.example.model.Edit;
 import com.example.service.BookService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 @RestController()
@@ -19,15 +22,18 @@ public class BooksController {
     @Autowired 
     BookService service;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/list") 
     List<Book> listBooks(){
+        logger.warn("** List books");
         return this.service.listBooks();
     }
 
 
     @GetMapping("/get/{id}")
     Edit getBook(@PathVariable(value="id") int id){
-        System.out.println(">>> ID: " + id);
+        logger.warn("** Get Book with id={}", id);
         Edit edit = new Edit();
         if (id < 0){
             edit.setBook(new Book());
